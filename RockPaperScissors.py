ab496dfd5958182961d825a44d3d2940e615e908
DevWithoutKnowledge <57572660+DevWithoutKnowledge@users.noreply.github.com>
2020-02-02 22:46:04 +0100

    now better

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,33 +1,18 @@
-import sys
+values = {"paper": "rock", "rock": "scissors", "scissors": "paper"}
+
 while True:
-    ansplayer1=input("Player 1, do you want to play a game? Yes/No ")
-    ansplayer2=input("Player 2, do you want to play a game? Yes/No ")
-    agree=["yes"]
-    if ansplayer1.lower() not in agree or ansplayer2.lower() not in agree:
+    ansplayer1 = input("Player 1, do you want to play a game? Yes/No ")
+    ansplayer2 = input("Player 2, do you want to play a game? Yes/No ")
+    if ansplayer1.lower() != "yes" or ansplayer2.lower() != "yes":
         print("One of you didn't want to play a game! :(")
-    u1=input("Choose paper/rock/scissors: ")
-    u2=input("Choose paper/rock/scissors: ")
-    def compare(u1, u2):
-        if u1==u2:
-            print("You both win!")
-        elif u1!="paper" or "rock" or "scissors" or u2!="paper" or "rock" or "scissors":
-            print("You chose wrong!")
-            sys.exit()
-        elif u1.lower()=="paper":
-            if u2.lower()=="rock":
-                print("Player 1 won!")
-            else:
-                print("Player 2 won!")
-        elif u1.lower()=="rock":
-            if u2.lower()=="paper":
-                print("Player 2 won!")
-            else:
-                print("Player 1 won!")
-        elif u1.lower()=="scissors":
-            if u2.lower()=="paper":
-                print("Player 1 won!")
-            else:
-                print("Player 2 won!")
-        else:
-            print("You made a bad decision! Try again.")
-    print(compare(u1,u2))
+        exit(0)
+    u1 = input("Player 1: Choose paper/rock/scissors: ")
+    u2 = input("Player 2: Choose paper/rock/scissors: ")
+    if u1 == u2:
+        print("You both win!")
+    elif values[u1] == u2:
+        print("Player 1 won!")
+    elif values[u2] == u1:
+        print("Player 2 won!")
+    else:
+        print("You made a bad decision! Try again.")
